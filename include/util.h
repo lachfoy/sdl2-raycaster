9fedc187bf484e205c83e6769c9eb3542fe98fbc
Lachlan Foy <lachie.foy7@gmail.com>
2022-06-07 22:40:37 +1000

    header guard

--- include/util.h
+++ include/util.h
@@ -1,4 +1,6 @@
-#pragma once
+#ifndef UTIL_H
+#define UTIL_H
+
 #include <stdint.h>
 
 uint32_t ARGB(uint32_t red, uint32_t green, uint32_t blue, uint32_t alpha);
@@ -8,3 +10,5 @@ static uint32_t s_randX = 123456789;
 static uint32_t s_randY = 362436069;
 static uint32_t s_randZ = 521288629;
 uint32_t FastRand(void);
+
+#endif
