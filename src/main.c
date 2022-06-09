6a78a36bdd010af1cd3a088fa8963735d57f9488
Lachlan Foy <lachie.foy7@gmail.com>
2022-06-09 11:46:14 +1000

    update main

--- src/main.c
+++ src/main.c
@@ -3,11 +3,11 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-#include "util.h"
+#include "utils.h"
 #include "map.h"
 #include "raycast.h"
 #include "defines.h"
-#include "render.h"
+#include "draw.h"
 
 int main(int argc, char *argv[])
 {
@@ -37,8 +37,15 @@ int main(int argc, char *argv[])
         return 1;
     }
 
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    if (renderer == NULL)
+    {
+        printf("%s\n", SDL_GetError());
+        return 1;
+    }
 
-    if (renderInit(window) != 0)
+    SDL_Texture* texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STREAMING, RENDER_WIDTH, RENDER_HEIGHT);
+    if (texture == NULL)
     {
         printf("%s\n", SDL_GetError());
         return 1;
@@ -49,7 +56,32 @@ int main(int argc, char *argv[])
     SDL_Event e;
     while(!quit)
     {
-        renderClear(0x00FF00);
+        // The Back Buffer texture may be stored with an extra bit of width (pitch) on the video card in order to properly
+        // align it in VRAM should the width not lie on the correct memory boundary (usually four bytes).
+        int32_t pitch = 0;
+
+        // This will hold a pointer to the memory position in VRAM where our Back Buffer texture lies
+        uint32_t *buffer = NULL;
+
+        // Lock the memory in order to write our Back Buffer image to it
+        if (!SDL_LockTexture(texture, NULL, (void **)&buffer, &pitch))
+        {
+            // The pitch of the Back Buffer texture in VRAM must be divided by four bytes
+            // as it will always be a multiple of four
+            pitch /= sizeof(uint32_t);
+
+            //draw_cls(renderer, texture, 0x0000FF);
+            draw_pixel(buffer, 50, 50, 0xFF0000);
+
+            // Unlock the texture in VRAM to let the GPU know we are done writing to it
+            SDL_UnlockTexture(texture);
+
+            // Copy our texture in VRAM to the display framebuffer in VRAM
+            SDL_RenderCopy(renderer, texture, NULL, NULL);
+
+            // Copy the VRAM framebuffer to the display
+            SDL_RenderPresent(renderer);
+        }
 
         while (SDL_PollEvent(&e))
         {
@@ -60,9 +92,12 @@ int main(int argc, char *argv[])
                 quit = true;
             }
         }
+
+        SDL_Delay(33.3);
     }
 
-    renderCleanup();
+    SDL_DestroyTexture(texture);
+    SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     
     SDL_Quit();
